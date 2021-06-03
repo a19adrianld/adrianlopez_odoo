e8ce0210dda4f765807136c1f6819e9c14f2e5b5
a19adrianld <a19adrianld@gmail.com>
2021-06-03 11:44:27 +0200

    Version 1.0

--- models/adriodoo.py
+++ models/adriodoo.py
@@ -7,9 +7,13 @@ class adriodoo(models.Model):
     _name = 'adrianlopez_odoo.adriodoo'
     _description = 'Prueba examen odoo'
 
-    name = fields.Char(string="Hola mundo")
+    #name = fields.Char(string="Hola mundo")
+    name = fields.Char(required=True, size=20, string="Suceso")
+    descripcion = fields.Text(string="A Descripción do Suceso")  # string é a etiqueta do campo
+    nivel = fields.Selection([('Baixo', 'Baixo'), ('Medio', 'Medio'), ('Alto', 'Alto')], string='Nivel')
+    data_hora = fields.Datetime(string="Data e Hora", default=lambda self: fields.Datetime.now())
 
-    autorizado = fields.Boolean(string="¿Autorizado?")
+    #autorizado = fields.Boolean(string="¿Autorizado?")
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
